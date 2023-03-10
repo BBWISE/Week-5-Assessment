64664851799a448d6eb3e9496ae3a94e3453df61
BBWISE <bayodeblessingakinbola@gmail.com>
2023-03-10 21:44:54 +0100

    Commit from BAYODE BLESSING AKINBOLA

--- JS/JavaScript.js
+++ JS/JavaScript.js
@@ -7,80 +7,80 @@ var operation = ""
 
 function num1() {
     let n = document.getElementById("inputresult").value;
-
+    
     let digit = n.toString() + 1;
-    document.getElementById("inputresult").innerHTML = digit;
+    document.getElementById("inputresult").value = Number(digit);
 }
 function num2() {
     let n = document.getElementById("inputresult").value;
 
     let digit = n.toString() + 2;
-    document.getElementById("inputresult").innerHTML = digit;
+    document.getElementById("inputresult").value = Number(digit);
 }
 function num3() {
     let n = document.getElementById("inputresult").value;
 
     let digit = n.toString() + 3;
-    document.getElementById("inputresult").innerHTML = digit;
+    document.getElementById("inputresult").value = Number(digit);
 }
 function num4() {
     let n = document.getElementById("inputresult").value;
 
     let digit = n.toString() + 4;
-    document.getElementById("inputresult").innerHTML = digit;
+    document.getElementById("inputresult").value = Number(digit);
 }
 function num5() {
     let n = document.getElementById("inputresult").value;
 
     let digit = n.toString() + 5;
-    document.getElementById("inputresult").innerHTML = digit;
+    document.getElementById("inputresult").value = Number(digit);
 }
 function num6() {
     let n = document.getElementById("inputresult").value;
 
     let digit = n.toString() + 6;
-    document.getElementById("inputresult").innerHTML = digit;
+    document.getElementById("inputresult").value = Number(digit);
 }
 function num7() {
     let n = document.getElementById("inputresult").value;
 
     let digit = n.toString() + 7;
-    document.getElementById("inputresult").innerHTML = digit;
+    document.getElementById("inputresult").value = Number(digit);
 }
 function num8() {
     let n = document.getElementById("inputresult").value;
 
     let digit = n.toString() + 8;
-    document.getElementById("inputresult").innerHTML = digit;
+    document.getElementById("inputresult").value = Number(digit);
 }
 function num9() {
     let n = document.getElementById("inputresult").value;
 
     let digit = n.toString() + 9;
-    document.getElementById("inputresult").innerHTML = digit;
+    document.getElementById("inputresult").value = Number(digit);
 }
 function num0() {
     let n = document.getElementById("inputresult").value;
 
     let digit = n.toString() + 0;
-    document.getElementById("inputresult").innerHTML = digit;
+    document.getElementById("inputresult").value = Number(digit);
 }
 function numDot() {
     let n = document.getElementById("inputresult").value;
 
     let digit = n.toString() + ".";
-    document.getElementById("inputresult").innerHTML = digit;
+    document.getElementById("inputresult").value = Number(digit);
 }
 function acButton() {
     answers = 0;
     number1 = "";
     operation = "";
-    document.getElementById("inputresult").innerHTML = "";
+    document.getElementById("inputresult").value = 0;
 }
 function percentageButton() {
     let n = document.getElementById("inputresult").value;
 
-    document.getElementById("inputresult").innerHTML = (n/100);
+    document.getElementById("inputresult").value = (n / 100);
 }
 function divideButton() {
     let n = document.getElementById("inputresult").value;
@@ -94,7 +94,7 @@ function divideButton() {
         operation = "/";
     }
 
-    document.getElementById("inputresult").innerHTML = "";
+    document.getElementById("inputresult").value = 0;
 }
 function addButton() {
     let n = document.getElementById("inputresult").value;
@@ -108,9 +108,9 @@ function addButton() {
         operation = "+";
     }
 
-    document.getElementById("inputresult").innerHTML = "";
+    document.getElementById("inputresult").value = 0;
 }
-function substractButton() {
+function subtractButton() {
     let n = document.getElementById("inputresult").value;
 
     if (number1 != "") {
@@ -122,7 +122,7 @@ function substractButton() {
         operation = "-";
     }
 
-    document.getElementById("inputresult").innerHTML = "";
+    document.getElementById("inputresult").value = 0;
 }
 function multiplyButton() {
     let n = document.getElementById("inputresult").value;
@@ -136,7 +136,7 @@ function multiplyButton() {
         operation = "*";
     }
 
-    document.getElementById("inputresult").innerHTML = "";
+    document.getElementById("inputresult").value = 0;
 }
 function equalsButton() {
     let n = document.getElementById("inputresult").value;
@@ -145,5 +145,5 @@ function equalsButton() {
     operation = "";
     number1 = "";
 
-    document.getElementById("inputresult").innerHTML = answers;
+    document.getElementById("inputresult").value = answers;
 }
